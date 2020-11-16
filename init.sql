9ab09a3791b9ca2ca1549fde811bfa59cd7d0055
erkhul6 <73967989+erkhul6@users.noreply.github.com>
2020-11-15 16:32:48 -0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -30,7 +30,7 @@ CREATE TABLE Employer(employerID int,
 			  UNIQUE(email),
 			  PRIMARY KEY(employerID),
 			  FOREIGN KEY(cID) REFERENCES Company(companyID)
-			ON DELETE CASCADE ON UPDATE CASCADE);	
+			ON DELETE NO ACTION ON UPDATE CASCADE);	
 
 CREATE TABLE E1(jobDescription VARCHAR(255),
 				eID int,
@@ -135,9 +135,6 @@ ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY(aID) REFERENCES Application(applicationID)
 ON DELETE CASCADE ON UPDATE CASCADE) ;
 
-				       
-				       
-				       
 INSERT INTO Application
 VALUES('2007-05-08 12:35:29.123', 18282);
 
@@ -145,9 +142,17 @@ INSERT INTO Company
 VALUES(10034,'google', "dont be evil");
 
 INSERT INTO Employer
-VALUES(18324, 'mzuckerberg@facebook.com', 'badopsec', 'mark zuckerberg', NULL);
-REPLACE INTO Employer
 VALUES(18324, 'mzuckerberg@facebook.com', 'badopsec', 'mark zuckerberg', 10034);
 
 INSERT INTO E1 
 VALUES('this job sucks', 18324, 'engineering');
+
+DELETE FROM Employer WHERE name = 'mark zuckerberg';
+INSERT INTO Employer
+VALUES(18324, 'mzuckerberg@facebook.com', 'badopsec', 'mark zuckerberg', 10034);
+
+
+
+
+
+

0f42f682105ddc9ace0e18b0d9666995ddea625b
erkhul6 <73967989+erkhul6@users.noreply.github.com>
2020-11-15 16:23:57 -0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -135,5 +135,20 @@ ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY(aID) REFERENCES Application(applicationID)
 ON DELETE CASCADE ON UPDATE CASCADE) ;
 
+				       
+				       
+				       
+INSERT INTO Application
+VALUES('2007-05-08 12:35:29.123', 18282);
 
+INSERT INTO Company
+VALUES(10034,'google', "dont be evil");
+
+INSERT INTO Employer
+VALUES(18324, 'mzuckerberg@facebook.com', 'badopsec', 'mark zuckerberg', NULL);
+REPLACE INTO Employer
+VALUES(18324, 'mzuckerberg@facebook.com', 'badopsec', 'mark zuckerberg', 10034);
+
+INSERT INTO E1 
+VALUES('this job sucks', 18324, 'engineering');
 

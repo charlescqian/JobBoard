38f8ae2a25fb48a1783e5d6ac380bbdf6286e180
charlescqian <charlesqian.ubc@gmail.com>
2020-11-16 00:04:29 -0800

    Removed unused code from Chart.js

--- client/src/Chart.js
+++ client/src/Chart.js
@@ -4,36 +4,6 @@ import { BarChart, Line, XAxis, YAxis, Label, ResponsiveContainer, CartesianGrid
 import Title from './Title';
 import { Button, ButtonGroup } from '@material-ui/core';
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
-const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-];
-  
-
 class Chart extends Component {
   constructor() {
     super()

eee51413b95b5ed8d389c1cfdaac600b17d8b2f1
charlescqian <charlesqian.ubc@gmail.com>
2020-11-23 15:13:39 -0800

    Removed unused imports in UpdateInsert

--- client/src/UpdateInsert.js
+++ client/src/UpdateInsert.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 import { Button, TextField, Grid, ButtonGroup } from '@material-ui/core';
 
